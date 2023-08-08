d9ffc8f79fa18256be90451f4f7d371a92508c4c
OosKarIn <oscar.dimetrix.97@gmail.com>
2023-08-07 22:11:21 -0600

    Better performance and scalable code
    Reducing the loops iterations and develop code scalability

--- Convert JTS-HTM to CSV.py
+++ Convert JTS-HTM to CSV.py
@@ -1,131 +1,120 @@
 ﻿#nombre a archivo csv
 #11 to fix bug jupiter bft
+# Using list List Comprehensions
 
 from bs4 import BeautifulSoup
 import datetime
+import time
 import numpy as np
+from numpy import asarray
 import csv
 import shutil, os
 from pathlib import Path
 import glob
 
+logFilesPath = ""
+globalHeaders = []
+globalLL = []
+globalUL = []
+dataScrap = ['\n', '•', '\xa0', '\r', '\t', ',']
+headersWritten = False
+totalFilesProcessed = 0
+file_name_flag = False
+csvFileName = ''
 
-
-def generarCsv():
-    head, tail = os.path.split(name)
-    print(tail)
-    tail=tail[:-4]
-    listaFinal = infoList + listaPruebas
-    listaFinal.insert(0,['ID',tail])
-    print(listaFinal)
-    csvName = listaFinal[4][1]
-    print(csvName)
-    tester = csvName[:-3]
-    #fixing bug for Jupiter
-    if tester == ' JUPITER_BFT':
-        listaFinal[78][1]=r'getprop ro.boot.secure_cpu\n1\njupiter ~>'
-        listaFinal[109][1] =r'getprop ro.boot.secure_cpu\n1\nganymede ~>'
-
-    column = []
-    column2 = []
-    for ii in range(len(listaFinal)):
-        xxx = listaFinal[ii][0]
-        column.append(xxx)
-
-    for j in range(len(listaFinal)):
-        yyy = listaFinal[j][1]
-        yyy = yyy.replace("\n","")
-        yyy = yyy.replace(",",";")
-        yyy = yyy.replace("\r","")
-        yyy = yyy.replace("#","")
-        column2.append(yyy)
-
-    column3 = [column , column2]
-    with open(csvName+'.csv','ab') as f:
-        #newresult = np.random.rand(2, 3)
-        np.savetxt(f, column3, delimiter="," , fmt="%s")
-        
-path = os.getcwd()
-print(path)
-fullPath = path + "\\html\\*.htm"
-for name in glob.glob(fullPath):
-#for name in glob.glob("C:\\TE\\TestLogs\\html\\*.htm"):   # Loop for search all the .htm files
-    print(name)
-    with open ( name ) as fp:                             # open the html
-        soup = BeautifulSoup(fp, "html.parser")
-
-        a = soup.find_all('h3')                            #find all the .htm H3
-        li = []
-        for i in range(len(a)):                             #Loop for all the h3
-            headers = a[i].text
-
-            li.append(headers)
-
-        li[7] = li[7][0:17]
-        li = li[1:]
-        infoList = []
-        for k in range(len(li)):
-            spl= li[k].split(":", 1)
-            infoList.append(spl)
-            #print(infoList)
-        b = soup.find_all('td')
-        liTd = []
-        for q in range(len(b)):
-            table = b[q].text
-            liTd.append(table)
-
-        liTd = liTd[12:]
-
-        dataName =[]
-        dataStatus =[]
-        dataValue =[]
-        for t in range(1,len(liTd),11):
-            dataName.append(liTd[t])
-
-        for y in range(2,len(liTd),11):
-            dataStatus.append(liTd[y])
-
-        for u in range(7,len(liTd),11):
-            dataValue.append(liTd[u])
-        listaVergas = []
-
-        for lista in range(len(dataName)):
-            x = dataName[lista], dataValue[lista], dataStatus[lista]
-            x = list(x)
-            listaVergas.append(x)
-        #print(listaVergas)
-        listaPruebas = listaVergas.copy()
-        for www in range(len(listaPruebas)):
-            listaPruebas[www] = listaPruebas[www][:2]
-        #print(listaPruebas)
-
-        m = infoList[1][1] =infoList[1][1].replace(",","")
-        n = infoList[2][1] = infoList[2][1].replace(",","")
-        infoList[1][1] = m[1:]
-        infoList[2][1] = n[1:]
-        #print(infoList[1][1])
-        date_time_obj = datetime.datetime.strptime(str(infoList[1][1]),"%A %B %d %Y %I:%M:%S %p")
-        infoList[1][1] = str(date_time_obj.date())+ " "+ str(date_time_obj.time())
-        date_time_obj = datetime.datetime.strptime(infoList[2][1],"%A %B %d %Y %I:%M:%S %p")
-        infoList[2][1] = str(date_time_obj.date())+ " "+ str(date_time_obj.time())
-        #print(infoList[2][1])
-
-        if (infoList[6][1]) == " FAIL":
-            print("Lo logramos")
-            z = 0
-            for danonino in range(len(listaVergas)):
-                padre = listaVergas[danonino]
-                for hijo in range(3):
-                    #print(padre[hijo])
-                    if(padre[hijo] == "FAIL"):
-                        if z == 0:
-                            laFalla = padre[0]
-                            z =z+1
-                        else:
-                            break
-            infoList[6][1] = laFalla
-            generarCsv()
-
+def generateCSVFileName():
+    current_time = time.localtime()
+    return time.strftime('%m-%d-%Y_%H-%M-%S', current_time)
+     
+def attachToCSV(file_Data):
+    global headersWritten
+    global file_name_flag
+    global csvFileName
+    #GET THE ID: with the TIAL Path file name
+    #GET THE CSV NAME: with the tester name and the actual timestamp
+    if not file_name_flag:
+        csvFileName = generateCSVFileName()
+        file_name_flag = True
+    #csvFileName = file_Data[4]
+    with open('CSV_' + csvFileName +'.csv','ab') as f:
+        if not headersWritten:
+            headersWritten = True
+            np.savetxt(f, (globalHeaders, file_Data), delimiter="," , fmt="%s")
         else:
-            infoList[6][1] = "PASS"
-            generarCsv()
+            np.savetxt(f, asarray([file_Data]), delimiter="," , fmt="%s")
+
+def fileScrapper(data):
+    for scrap in dataScrap:
+        data = data.replace(scrap, '')
+    return data
+
+def htmTimeFormarterForCSV(dateTime):
+    date_time_obj = datetime.datetime.strptime(str(dateTime),"%A %B %d %Y %I:%M:%S %p")
+    return str(date_time_obj.date())+ " "+ str(date_time_obj.time())    
+
+def filesProcessing():
+    
+    global globalHeaders
+    global globalLL
+    global globalUL
+    global headersWritten
+    global totalFilesProcessed
+    
+    for filePathName in glob.glob(logFilesPath):
+        file_Data_Table = [[],[],[],[],[],[],[],[],[],[],[],]
+        #TestGroup,TestName,TestStatus,Parametrics,IsMeasurement,LowerLimit,UpperLimit,Measurement,MeasurementUnit,ElapsedTime,ExecutionTime
+        file_Headers = ['ID']
+        file_Data = []
+        file_LL = []
+        file_UL = []
+        file_Data.append(os.path.split(filePathName)[1][:-4])
+        
+        with open (filePathName) as currentFile:
+            bsFile = BeautifulSoup(currentFile, "html.parser")
+            
+            allH3Tags = bsFile.find_all('h3')
+            for tagH3 in allH3Tags:
+                if ':' in tagH3.text:
+                    file_Headers.append(fileScrapper(tagH3.text[ : tagH3.text.find(':')]))
+                    if file_Headers[len(file_Headers)-1] in 'Test Result':
+                        file_Data.append(fileScrapper(tagH3.text[tagH3.text.find(':') + 2 : tagH3.text.find('\n')]))
+                    else:
+                        file_Data.append(fileScrapper(tagH3.text[tagH3.text.find(':') + 2 : ]))
+                        
+            file_Data[2] = htmTimeFormarterForCSV(file_Data[2])
+            file_Data[3] = htmTimeFormarterForCSV(file_Data[3])
+            
+            allTDTags = bsFile.find_all('td')
+            secondTR = bsFile.find_all('center', limit=13)
+            del secondTR[:2]
+            del allTDTags[:12]
+            i = 0
+            while len(allTDTags) > 0:
+                file_Data_Table[i].append(fileScrapper(allTDTags.pop(0).text))
+                i+=1
+                if i==len(secondTR):
+                    i=0
+            
+            file_Data.extend(file_Data_Table[7])
+            file_Headers.extend(file_Data_Table[1])
+            
+            #Getting the perfect headers that will be the global ones, only 1 header for all csv file.
+            if len(file_Headers) > len(globalHeaders):
+                headersWritten = False
+                globalHeaders = file_Headers.copy()
+                globalLL = file_Data_Table[5].copy()
+                globalUL = file_Data_Table[6].copy()
+            
+            attachToCSV(file_Data)
+            
+            print(file_Data[0])
+            totalFilesProcessed += 1
+            
+def main():
+    global logFilesPath
+    path = os.getcwd()
+    logFilesPath = path + "\\html\\*.htm"    
+    filesProcessing()
+    print('Total Files Proccesed:', totalFilesProcessed)
+        
+main()
